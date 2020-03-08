ef677324640470e403332568b5407744fb5daad3
Katie88 <kathrynhoth@gmail.com>
2020-03-08 11:59:00 -0600

    rest of styles and updated service worker

--- src/App.js
+++ src/App.js
@@ -1,72 +1,36 @@
-import React, {useState} from 'react';
-import { Container } from "reactstrap"; //, Row, Col
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
-
-import NavBar from "./components/NavBar";
-import EmpSearch from "./components/empSearch";
-import EmpCard from "./components/empCard";
-import sampleData from "./data/sample.json";
-
+import "./App.css";
 
 function App() {
-  const [employee, setEmployee] = useState({
-    searchTerm: "",
-    data: sampleData.results,
-    searchResults: []
-  })
-
-  const { searchTerm, data, searchResults } = employee;
-
-  const handleInputChange = e => {
-    const searchResults = data.filter(employee => employee.email.toLowerCase().includes(e.target.value.toLowerCase()));
-    setEmployee({ ...employee, searchResults, searchTerm: e.target.value});
-  }
-
-  const returnResults = () => {
-    if (searchResults.length) {
-
-      return searchResults.map(record => (
-        <EmpCard
-        key={record.registered.date}
-        id={record.registered.date}
-        image = {record.picture.large}
-        first = {record.name.first}
-        last = {record.name.last}
-        phone={record.phone}
-        email = {record.email}
-        
-        />
-      ))
-    } else {
-
-      return data.map(record => (
-      <EmpCard
-      key={record.registered.date}
-      id={record.registered.date}
-      image = {record.picture.large}
-      first = {record.name.first}
-      last = {record.name.last}
-      phone={record.phone}
-      email = {record.email}
-     
-      />
-    ))
-
-    }
-    
-  }
-
   return (
     <div className="App">
-      <NavBar />
-      <EmpSearch
-        searchTerm={searchTerm}
-        handleInputChange={handleInputChange}
-      />
-      
-      <Container>
-        {returnResults()}
-      </Container>
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
